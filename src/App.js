42058d366afb3dab7614ed28921e2255d66d9626
LindsayH <lindsaynhenderson@gmail.com>
2023-03-02 11:05:17 -0600

    add bootstrap

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react'
+import { Route, Routes, Link } from 'react-router-dom'
+import './App.css'
 
 function App() {
+  const [contacts, setContacts] = useState([])
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        Ducks
+        <nav>
+          <Link to='/'>Contact List</Link>
+          <Link className='m-left' to='/add'>Add Contact</Link>
+        </nav>
       </header>
+      <main>
+        
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
