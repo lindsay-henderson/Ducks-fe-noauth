bde34f7f293bf2091e9c0e8e0a07b56a6a08cc69
LindsayH <lindsaynhenderson@gmail.com>
2023-03-12 16:01:39 -0500

    contact list is displayong, starting to organize bt order date

---
+++ src/pages/OrderList/OrderDisplay.jsx
@@ -0,0 +1,9 @@
+import OrderList from './pages/OrderList/OrderList.jsx'
+import styles from './OrderList.module.css'
+
+function Display(props)
+
+return (
+  Props.orders.sort
+)
+
--- src/pages/OrderList/OrderList.jsx
+++ src/pages/OrderList/OrderList.jsx
@@ -1,21 +1,25 @@
 import styles from './OrderList.module.css'
 
 function OrderList(props) {
+
+
   return (
     <>
       <h1>Order List</h1>
       <div className={styles.container}>
-        {props.orders.map(order => (
-          <div key={order._id}>
+          {props.orders.map(order  => (
+            <div key={order._id}>
             <p>Order Name: {order.name}</p>
             <p>client: {order.client}</p>
             <p>Image Folder: {order.photos}</p>
             <p>Notes <br/> {order.notes}</p>
+            <p>{order.orderDate}</p>
           </div>
-        ))}
+          ))}
       </div>
     </>
   )
+
 }
 
 export default OrderList
